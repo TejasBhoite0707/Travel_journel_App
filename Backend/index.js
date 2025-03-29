c4194f681372c2176cc6cfa1105a3e2ee1ba6d89
TejasBhoite0707 <tejasbhoite3006@gmail.com>
2025-03-29 20:21:34 +0530

    Login API Done!

--- Backend/index.js
+++ Backend/index.js
@@ -60,6 +60,39 @@ return res.status(201).json({
 })
 
 
+app.post('/login',async(req,res)=>{
+    const{email,password}=req.body;
+
+    if(!email || !password){
+        return res.status(400).json({message:"Please fill teh eamil and password"})
+    }
+
+    const user=await userModal.findOne({email});
+    if(!user){
+        return res.status(400).json({message:"User Not Found"});
+    }
+
+    const isPasswordValid=await bcrypt.compare(password,user.password);
+    if(!isPasswordValid){
+        return res.status(400).json({message:"Invalid Credintionals"});
+    }
+
+    const accessTokenLogin=jwt.sign(
+        {userId:user._id},
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:"72h",
+        }
+    )
+     
+    return res.status(400).json({error:false,
+        message:"Login Successfull",
+    user:{fullname:user.fullname,email:user.email},
+    accessTokenLogin
+    })
+
+})
+
 let port=8000
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
