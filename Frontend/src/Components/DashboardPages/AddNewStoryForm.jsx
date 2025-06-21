5958e2fc5829f869b7b8bb39087f9248bce7acc8
TejasBhoite0707 <tejasbhoite3006@gmail.com>
2025-06-21 17:17:21 +0530

    Edit Story Integration Done

--- Frontend/src/Components/DashboardPages/AddNewStoryForm.jsx
+++ Frontend/src/Components/DashboardPages/AddNewStoryForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import {
   Button,
@@ -17,44 +17,73 @@ import {
 } from 'antd';
 import FormItem from 'antd/es/form/FormItem';
 import axiosInstance from '../../utils/ApiService';
-const NewStoryForm = ({form}) => {
-   
-    const normFile = e => {
-        if (Array.isArray(e)) {
-          return e;
-        }
-        return e === null || e === void 0 ? void 0 : e.fileList;
-      };
-
-      const onFinish=async(values)=>{
-        try {
-           const payload = {
-        title: values.title,
-        story: values.story,
-        
-        visitedLocation: values.visitedLocation,
-        visitedDate: values.visitedDate?.format('YYYY-MM-DD'),
-        isFavourite: values.isFavourite || false,
-        imageUrl: values.image[0].response.imageUrl,
-      };
-      console.log(payload);
-      
-      const response=await axiosInstance.post('/api/travel-story-add',payload)
-      console.log(response);
-      message.success('Story added successfully!');
-      form.resetFields();
-        } catch (error) {
-           console.error('Error submitting form:', error);
-      
-        }
-      }
+import dayjs from 'dayjs';
+import toast from 'react-hot-toast';
+const NewStoryForm = ({ form, mode = 'add', initialValues = {}, onSuccess }) => {
   const [componentSize, setComponentSize] = useState('default');
+  useEffect(() => {
+    if (mode === 'edit' && initialValues) {
+      const transformedValues = {
+        ...initialValues,
+        visitedDate: initialValues.visitedDate ? dayjs(initialValues.visitedDate) : null,
+        visitedLocation: initialValues.visitedLocation || [],
+        isFavourite: initialValues.isFavourite || false,
+        image: initialValues.imageUrl ? [{ url: initialValues.imageUrl, name: 'Uploaded Image', status: 'done' }] : [],
+      }
+      console.log(transformedValues);
+      form.resetFields()
+      form.setFieldsValue(transformedValues);
+      console.log(transformedValues);
+      
+    }
+    
+    
+  }, [initialValues, form, mode]);
+ 
+  const normFile = e => {
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e === null || e === void 0 ? void 0 : e.fileList;
+  };
+
+  const onFinish = async (values) => {
+
+    const payload = {
+      title: values.title,
+      story: values.story,
+
+      visitedLocation: values.visitedLocation,
+      visitedDate: values.visitedDate?.format('YYYY-MM-DD'),
+      isFavourite: values.isFavourite || false,
+      imageUrl: values.image[0].url,
+    };
+     
+
+   
+    try {
+      if (mode === 'edit') {
+        await axiosInstance.put(`api/edit-travel/${initialValues._id}`, payload)
+        toast.success('Story updated successfully!')
+      }
+      else {
+        await axiosInstance.post('/api/travel-story-add', payload);
+        toast.success('Story Added successfully!')
+      }
+      form.resetFields();
+      onSuccess?.();
+    } catch (error) {
+      toast.error("Something Went wrong!")
+    }
+
+  }
+
   const onFormLayoutChange = ({ size }) => {
     setComponentSize(size);
   };
   return (
     <Form
-    form={form}
+      form={form}
       labelCol={{ span: 6 }}
       wrapperCol={{ span: 16 }}
       layout="horizontal"
@@ -62,83 +91,83 @@ const NewStoryForm = ({form}) => {
       onFinish={onFinish}
       onValuesChange={onFormLayoutChange}
       size={componentSize}
-      
+
     >
-      
-      <Form.Item label="Title" name='title' rules={[{required:true}]}>
+
+      <Form.Item label="Title" name='title' rules={[{ required: true }]}>
         <Input />
       </Form.Item>
 
-      <Form.Item label="Story" name='story' rules={[{required:true}]}>
-         <Input.TextArea rows={4}/>
+      <Form.Item label="Story" name='story' rules={[{ required: true }]}>
+        <Input.TextArea rows={4} />
       </Form.Item>
 
-<Form.List name="visitedLocation" rules={[{required:true}]}>
-       {(fields,{add,remove})=>(
-        <Form.Item label='Visited Locations' required>
-        <>
-        {fields.map(({key,name,...restField})=>(
-          <Space>
-            <Form.Item
-            {...restField}
-            name={name}
-            rules={[{required:true,message:"Enter A Location"}]}
-            noStyle
-            >
-<Input placeholder='Enter the Place name' style={{width:'250px'}}/>
-            </Form.Item>
-            <MinusCircleOutlined onClick={()=>remove(name)}/>
-          </Space>
-        ))}
-<Form.Item wrapperCol={{offset:6}}>
-<Button type='dashed' onClick={()=>add()} block icon={<PlusOutlined/>} >
-Add Place
-</Button>
-</Form.Item>
-        </>
-        </Form.Item>
-       )}
+      <Form.List name="visitedLocation" rules={[{ required: true }]}>
+        {(fields, { add, remove }) => (
+          <Form.Item label='Visited Locations' required>
+            <>
+              {fields.map(({ key, name, ...restField }) => (
+                <Space>
+                  <Form.Item
+                    {...restField}
+                    name={name}
+                    rules={[{ required: true, message: "Enter A Location" }]}
+                    noStyle
+                  >
+                    <Input placeholder='Enter the Place name' style={{ width: '250px' }} />
+                  </Form.Item>
+                  <MinusCircleOutlined onClick={() => remove(name)} />
+                </Space>
+              ))}
+              <Form.Item wrapperCol={{ offset: 6 }}>
+                <Button type='dashed' onClick={() => add()} block icon={<PlusOutlined />} >
+                  Add Place
+                </Button>
+              </Form.Item>
+            </>
+          </Form.Item>
+        )}
       </Form.List>
 
-<Form.Item label="Date Visited" name='visitedDate' rules={[{required:true}]}>
+      <Form.Item label="Date Visited" name='visitedDate' rules={[{ required: true }]}>
         <DatePicker />
       </Form.Item>
 
       <Form.Item label="Upload Image" name='image' valuePropName="fileList" getValueFromEvent={normFile}>
-          <Upload name='image'
-          customRequest={async({file,onSuccess,onError})=>{
-         const formData=new FormData();
-         formData.append('image',file);
-         try {
-          const response=await axiosInstance.post('/api/image-upload',formData,{
-            headers:{
-              "Content-Type":'multipart/form-data'
+        <Upload name='image'
+          customRequest={async ({ file, onSuccess, onError }) => {
+            const formData = new FormData();
+            formData.append('image', file);
+            try {
+              const response = await axiosInstance.post('/api/image-upload', formData, {
+                headers: {
+                  "Content-Type": 'multipart/form-data'
+                }
+              })
+              onSuccess(response.data, file)
+            } catch (err) {
+              console.error(err);
+              onError(err)
             }
-          })
-          onSuccess(response.data,file)
-         } catch (err) {
-          console.error(err);
-          onError(err)
-         }
           }}
-          
+
           listType="picture-card">
-            <div
-              style={{ color: 'inherit', cursor: 'inherit', border: 0, background: 'none' }}
-              type="button"
-            >
-              <PlusOutlined />
-              <div style={{ marginTop: 8 }}>Upload</div>
-            </div>
-          </Upload>
-        </Form.Item>
-      
-     
-      <Form.Item  valuePropName="checked" name='isFavourite' label='Favourite'>
-        <Switch />
+          <div
+            style={{ color: 'inherit', cursor: 'inherit', border: 0, background: 'none' }}
+            type="button"
+          >
+            <PlusOutlined />
+            <div style={{ marginTop: 8 }}>Upload</div>
+          </div>
+        </Upload>
       </Form.Item>
 
-      
+
+      <Form.Item valuePropName="checked" name='isFavourite' label='Favourite'>
+        <Switch />
+      </Form.Item>
+     
+
     </Form>
   );
 };
--- Frontend/src/Components/DashboardPages/TimeLineComponent.jsx
+++ Frontend/src/Components/DashboardPages/TimeLineComponent.jsx
@@ -1,81 +1,122 @@
 import React, { useEffect, useState } from 'react';
-import { Empty, Spin, Timeline } from 'antd';
+import { Empty, Form, Input, Modal, Spin, Timeline } from 'antd';
 import Timelinecard from './Timelinecard';
-import { MdOutlineDateRange  } from "react-icons/md";
+import { MdOutlineDateRange } from "react-icons/md";
 import axiosInstance from '../../utils/ApiService';
+import toast, { Toaster } from 'react-hot-toast'
 import moment from 'moment';
+import FormItem from 'antd/es/form/FormItem';
+import NewStoryForm from './AddNewStoryForm';
 const TimeLinecomponent = () => {
-  const[memories,setMemories]=useState([]);
-  const[loading,setloading]=useState(true);
+  const [memories, setMemories] = useState([]);
+  const [loading, setloading] = useState(true);
+  const [editingStory, setEditingStory] = useState(null)
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false)
+  const [form] = Form.useForm();
+  const[formKey,setFormKey]=useState(0)
 
-    const fetchMemories=async()=>{
-    
+  const fetchMemories = async () => {
+
     try {
-      const response=await axiosInstance.get('/api/get-all-stories');
+      const response = await axiosInstance.get('/api/get-all-stories');
       setMemories(response.data.stories);
       console.log(response.data);
-      
+
     } catch (err) {
       console.error(err);
     }
-    finally{
+    finally {
       setloading(false)
     }
 
-    }
-  useEffect(()=>{
-    fetchMemories();
-  },[])
-  
-const UpdateIsFavourite=async(storyId,newValue)=>{
-  try {
-    await axiosInstance.put(`/api/update-is-favourite/${storyId}`,{
-      isFavourite:newValue
-    });
-    setMemories((prev)=>
-    prev.map((m)=>
-    m._id===storyId?{...m,isFavourite:newValue}:m
-    )
-    );
-
-  } catch (err) {
-    console.error(err);
-    
   }
+  useEffect(() => {
+    fetchMemories();
+  }, [])
+
+  const UpdateIsFavourite = async (storyId, newValue) => {
+    try {
+      await axiosInstance.put(`/api/update-is-favourite/${storyId}`, {
+        isFavourite: newValue
+      });
+      setMemories((prev) =>
+        prev.map((m) =>
+          m._id === storyId ? { ...m, isFavourite: newValue } : m
+        )
+      );
+
+    } catch (err) {
+      console.error(err);
+
+    }
+  }
+
+  console.log(memories);
+
+
+  if (loading) return <Spin tip="Loading travel stories..."></Spin>
+  if (!memories.length) return <Empty description="No memories yet!" />;
+
+  const handleEditClick = (storyData) => {
+    setEditingStory(storyData);
+    setFormKey(prev=>prev+1)
+    setIsEditModalOpen(true)
+  }
+const handleCancel=()=>{
+
+  setIsEditModalOpen(false);
 }
 
-    console.log(memories);
-    
-  
-  if(loading) return <Spin tip="Loading travel stories..."></Spin>
-  if(!memories.length) return <Empty description="No memories yet!" />;
-  
-  
+
   return (
     <>
-   
-     <Timeline
-  mode='left'
-  items={[...memories]
-    .sort((a, b) => new Date(b.visitedDate) - new Date(a.visitedDate)) // ✅ descending order
-    .map((memory) => ({
-      label: `${memory.visitedDate ? moment(memory.visitedDate).format("Do MMM YYYY") : ""}`,
-      dot: <MdOutlineDateRange className='bg-transparent' />,
-      children: (
-        <Timelinecard
-          title={memory.title}
-          story={memory.story}
-          visitedLocations={memory.visitedLocation}
-          imageUrl={memory.imageUrl}
-          isFavourite={memory.isFavourite}
-          onFavouriteToggle={(newValue)=>
-            UpdateIsFavourite(memory._id,newValue)
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+      />
+      <Timeline
+        mode='left'
+        items={[...memories]
+          .sort((a, b) => new Date(b.visitedDate) - new Date(a.visitedDate)) // ✅ descending order
+          .map((memory) => ({
+            label: `${memory.visitedDate ? moment(memory.visitedDate).format("Do MMM YYYY") : ""}`,
+            dot: <MdOutlineDateRange className='bg-transparent' />,
+            children: (
+              <Timelinecard
+                title={memory.title}
+                story={memory.story}
+                visitedLocations={memory.visitedLocation}
+                imageUrl={memory.imageUrl}
+                isFavourite={memory.isFavourite}
+                onFavouriteToggle={(newValue) =>
+                  UpdateIsFavourite(memory._id, newValue)
+                }
+                onEdit={() => handleEditClick(memory)}
+              />
+            ),
+          }))}
+      ></Timeline>
+      <Modal
+        title='Edit Travel Story'
+        open={isEditModalOpen}
+        key={formKey}
+        onCancel={handleCancel}
+        onOk={()=>form.submit()}
+        okText="Update Story"
+      >
+        <NewStoryForm
+          form={form}
+          mode='edit'
+          initialValues={editingStory}
+          onSuccess={() => {
+            setIsEditModalOpen(false)
+            fetchMemories()
+          }
+
           }
         />
-      ),
-    }))}
-></Timeline>
 
+      </Modal>
     </>
   );
 };
