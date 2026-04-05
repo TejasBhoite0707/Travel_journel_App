e13752ab1550acfb8dff05ae9b92234106539578
TejasBhoite0707 <tejasbhoite3006@gmail.com>
2026-04-06 01:31:11 +0530

    Last Check for CI Flow

--- Backend/Routes/ChangePassR.js
+++ Backend/Routes/ChangePassR.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import ChangePasswordMode from '../Controllers/ChangePassword.js'
-
+import authenticateToken from '../utilities.js';
 let ChangePasswordRouter=express.Router();
 
-ChangePasswordRouter.put('/change-password',ChangePasswordMode);
+ChangePasswordRouter.put('/change-password',authenticateToken,ChangePasswordMode);
 
 export default ChangePasswordRouter;
